f598a34f7df12d3e0c00e13cb9a885acdfe7fb04
MAXWELL.COSTA <maxwell880@gmail.com>
2021-05-10 19:38:01 -0300

    Adicionado as depencias no projeto

---
+++ src/main/java/com/maxwell/cursomc/domain/Categoria.java
@@ -0,0 +1,71 @@
+package com.maxwell.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Categoria")
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String nome;
+
+	public Categoria() {
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/maxwell/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/maxwell/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.maxwell.cursomc.resources;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,17 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maxwell.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String findAll() {
-		//código teste
-		List<String>  test = new ArrayList<String>();
-		test.add("INFORMATICA");
-		test.add("LIVROS");
-		test.add("ELETRONICOS");	
-		return "Return : "  + test.toString();
+	public List<Categoria> findAll() {
+		
+		Categoria cat1 = new Categoria();
+		cat1.setId(1000);
+		cat1.setNome("ELETRODOMESTICOS");
+
+		Categoria cat2 = new Categoria();
+		cat2.setId(1002);
+		cat2.setNome("INFORMATICA");
+
+		List<Categoria> categoria = new ArrayList<>();
+		categoria.add(cat1);
+		categoria.add(cat2);
+		return categoria;
 	}
 }
