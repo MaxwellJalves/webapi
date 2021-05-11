666c0098f6966ad9ccd8431b442250da010810c1
MAXWELL.COSTA <maxwell880@gmail.com>
2021-05-11 18:51:15 -0300

    Ajuste na porta do tomcat, adicionado os endpoints disponivel ao acessar o localhost:port do to cat

--- src/main/java/com/maxwell/cursomc/controller/Default.java
+++ src/main/java/com/maxwell/cursomc/controller/Default.java
@@ -10,6 +10,13 @@ public class Default {
 	@RequestMapping("/")
 	@ResponseBody
 	public String getIndex() {
-		return "<h2>Conexão Realizada com sucesso! <h2>";
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ul>")
+		.append("</br>").append("<li>").append("https/localhost:8082/categorias/{id}").append("</li>")
+		.append("</br>").append("<li>").append("https/localhost:8082/categorias").append("</li>")
+		.append("</br>").append("<li>").append("https/localhost:8082/produtos/{id}").append("</li>")
+		.append("</br>").append("<li>").append("https/localhost:8082/produtos/{name}").append("</li>")
+		.append("</br>").append("</ul>");
+		return sb.toString();
 	}
 }
